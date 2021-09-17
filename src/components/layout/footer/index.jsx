31fbddfc48df9acb701f4040b66dcdc544a67724
MarceloCopam <marcelo@copaminformatica.com.br>
2021-09-17 14:16:59 -0300

    fix: correct url

--- src/components/layout/footer/index.jsx
+++ src/components/layout/footer/index.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
         <footer className="relative bottom-0 flex items-center justify-center w-full h-24 border-t">
         <a
           className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://marcelo-portifolio-nextjs-marcelofullstack.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
